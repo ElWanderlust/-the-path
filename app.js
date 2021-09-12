70d31b8410e9ac0135b4758c785e7cf6c48bb89a
ElWanderlust <90190167+ElWanderlust@users.noreply.github.com>
2021-09-12 15:52:10 +0100

    cool button thing from earlier with audio working

--- app.js
+++ app.js
@@ -1,7 +1,141 @@
-function playAudio() {
-    const audio = document.getElementById(element);
-    audio.play();
-}
+//Stoic
 
+$('#getStoic').click(function() {
+    $('#stoicAudio').trigger('play');
+})
 
+$('#pauseStoic').click(function() {
+    $('#stoicAudio').trigger('pause');
+})
+$('#restartStoic').click(function() {
+    let vid = document.getElementById("stoicAudio");
+    vid.currentTime = 0;
+})
 
+$('#2XStoic').click(function() {
+    let vid = document.getElementById("stoicAudio");
+    vid.playbackRate = 2.0;
+})
+
+$('#1.5XStoic').click(function() {
+    let vid = document.getElementById("stoicAudio");
+    vid.playbackRate = 1.5;
+})
+
+$('#1XStoic').click(function() {
+    let vid = document.getElementById("stoicAudio");
+    vid.playbackRate = 1.0;
+})
+
+//SimpCheck
+$('#getSimpCheck').click(function() {
+    $('#simpAudio').trigger('play');
+})
+
+$('#pauseSimpCheck').click(function() {
+    $('#simpAudio').trigger('pause');
+})
+$('#restartSimpCheck').click(function() {
+    let vid = document.getElementById("simpAudio");
+    vid.currentTime = 0;
+})
+
+$('#2XSimpCheck').click(function() {
+    let vid = document.getElementById("simpAudio");
+    vid.playbackRate = 2.0;
+})
+
+$('#1.5XSimpCheck').click(function() {
+    let vid = document.getElementById("simpAudio");
+    vid.playbackRate = 1.5;
+})
+
+$('#1XSimpCheck').click(function() {
+    let vid = document.getElementById("simpAudio");
+    vid.playbackRate = 1.0;
+})
+//Zen
+$('#getZen').click(function() {
+    $('#Zen').trigger('play');
+})
+
+$('#pauseZen').click(function() {
+    $('#Zen').trigger('pause');
+})
+$('#restartZen').click(function() {
+    let vid = document.getElementById("Zen");
+    vid.currentTime = 0;
+})
+
+$('#2XZen').click(function() {
+    let vid = document.getElementById("Zen");
+    vid.playbackRate = 2.0;
+})
+
+$('#1.5XZen').click(function() {
+    let vid = document.getElementById("Zen");
+    vid.playbackRate = 1.5;
+})
+
+$('#1XZen').click(function() {
+    let vid = document.getElementById("Zen");
+    vid.playbackRate = 1.0;
+})
+
+//Limitless1
+
+$('#getLimitless1').click(function() {
+    $('#Limitless1').trigger('play');
+})
+
+$('#pauseLimitless1').click(function() {
+    $('#Limitless1').trigger('pause');
+})
+$('#restartLimitless1').click(function() {
+    let vid = document.getElementById("Limitless1");
+    vid.currentTime = 0;
+})
+
+$('#2XLimitless1').click(function() {
+    let vid = document.getElementById("Limitless1");
+    vid.playbackRate = 2.0;
+})
+
+$('#1.5XLimitless1').click(function() {
+    let vid = document.getElementById("Limitless1");
+    vid.playbackRate = 1.5;
+})
+
+$('#1XLimitless1').click(function() {
+    let vid = document.getElementById("Limitless1");
+    vid.playbackRate = 1.0;
+})
+
+//Limitless2
+
+$('#getLimitless2').click(function() {
+    $('#Limitless2').trigger('play');
+})
+
+$('#pauseLimitless2').click(function() {
+    $('#Limitless2').trigger('pause');
+})
+$('#restartLimitless2').click(function() {
+    let vid = document.getElementById("Limitless2");
+    vid.currentTime = 0;
+})
+
+$('#2XLimitless2').click(function() {
+    let vid = document.getElementById("Limitless2");
+    vid.playbackRate = 2.0;
+})
+
+$('#1.5XLimitless2').click(function() {
+    let vid = document.getElementById("Limitless2");
+    vid.playbackRate = 1.5;
+})
+
+$('#1XLimitless2').click(function() {
+    let vid = document.getElementById("Limitless2");
+    vid.playbackRate = 1.0;
+})
