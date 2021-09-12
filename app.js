03f71aa38bd99bca5f11597e686b6b6b983653fb
ElWanderlust <90190167+ElWanderlust@users.noreply.github.com>
2021-09-12 20:40:47 +0100

    Finalisation of Audio page and js

--- app.js
+++ app.js
@@ -119,34 +119,158 @@ $('#1XLimitless1').click(function() {
     vid.playbackRate = 1.0;
 })
 
-//Limitless2
+//NoSurf
 
-$('#getLimitless2').click(function() {
-    $('#Limitless2').trigger('play');
-    $('#Limitless2').css('display', 'flex');
+$('#getNoSurf').click(function() {
+    $('#NoSurf').trigger('play');
+    $('#NoSurf').css('display', 'flex');
 })
 
-$('#pauseLimitless2').click(function() {
-    $('#Limitless2').trigger('pause');
+$('#pauseNoSurf').click(function() {
+    $('#NoSurf').trigger('pause');
 })
-$('#restartLimitless2').click(function() {
-    let vid = document.getElementById("Limitless2");
+$('#restartNoSurf').click(function() {
+    let vid = document.getElementById("NoSurf");
     vid.currentTime = 0;
-    $('#Limitless2').css('display', 'none');
+    $('#NoSurf').css('display', 'none');
 })
 
-$('#2XLimitless2').click(function() {
-    let vid = document.getElementById("Limitless2");
+$('#2XNoSurf').click(function() {
+    let vid = document.getElementById("NoSurf");
     vid.playbackRate = 2.0;
 })
 
-$('#1.5XLimitless2').click(function() {
-    let vid = document.getElementById("Limitless2");
+$('#1.5XNoSurf').click(function() {
+    let vid = document.getElementById("NoSurf");
     vid.playbackRate = 1.5;
 })
 
-$('#1XLimitless2').click(function() {
-    let vid = document.getElementById("Limitless2");
+$('#1XNoSurf').click(function() {
+    let vid = document.getElementById("NoSurf");
+    vid.playbackRate = 1.0;
+})
+
+//NoSurf
+
+$('#getBloomer').click(function() {
+    $('#Bloomer').trigger('play');
+    $('#Bloomer').css('display', 'flex');
+})
+
+$('#pauseBloomer').click(function() {
+    $('#Bloomer').trigger('pause');
+})
+$('#restartBloomer').click(function() {
+    let vid = document.getElementById("Bloomer");
+    vid.currentTime = 0;
+    $('#Bloomer').css('display', 'none');
+})
+
+$('#2XBloomer').click(function() {
+    let vid = document.getElementById("Bloomer");
+    vid.playbackRate = 2.0;
+})
+
+$('#1.5XBloomer').click(function() {
+    let vid = document.getElementById("Bloomer");
+    vid.playbackRate = 1.5;
+})
+
+$('#1XBloomer').click(function() {
+    let vid = document.getElementById("Bloomer");
+    vid.playbackRate = 1.0;
+})
+
+//Lost
+
+$('#getLost').click(function() {
+    $('#Lost').trigger('play');
+    $('#Lost').css('display', 'flex');
+})
+
+$('#pauseLost').click(function() {
+    $('#Lost').trigger('pause');
+})
+$('#restartLost').click(function() {
+    let vid = document.getElementById("Lost");
+    vid.currentTime = 0;
+    $('#Lost').css('display', 'none');
+})
+
+$('#2XLost').click(function() {
+    let vid = document.getElementById("Lost");
+    vid.playbackRate = 2.0;
+})
+
+$('#1.5XLost').click(function() {
+    let vid = document.getElementById("Lost");
+    vid.playbackRate = 1.5;
+})
+
+$('#1XLost').click(function() {
+    let vid = document.getElementById("Lost");
+    vid.playbackRate = 1.0;
+})
+
+//Anxious
+
+$('#getAnxious').click(function() {
+    $('#Anxious').trigger('play');
+    $('#Anxious').css('display', 'flex');
+})
+
+$('#pauseAnxious').click(function() {
+    $('#Anxious').trigger('pause');
+})
+$('#restartAnxious').click(function() {
+    let vid = document.getElementById("Anxious");
+    vid.currentTime = 0;
+    $('#Anxious').css('display', 'none');
+})
+
+$('#2XAnxious').click(function() {
+    let vid = document.getElementById("Anxious");
+    vid.playbackRate = 2.0;
+})
+
+$('#1.5XAnxious').click(function() {
+    let vid = document.getElementById("Anxious");
+    vid.playbackRate = 1.5;
+})
+
+$('#1XAnxious').click(function() {
+    let vid = document.getElementById("Anxious");
+    vid.playbackRate = 1.0;
+})
+
+//Judgement
+
+$('#getJudgement').click(function() {
+    $('#Judgement').trigger('play');
+    $('#Judgement').css('display', 'flex');
+})
+
+$('#pauseJudgement').click(function() {
+    $('#Judgement').trigger('pause');
+})
+$('#restartJudgement').click(function() {
+    let vid = document.getElementById("Judgement");
+    vid.currentTime = 0;
+    $('#Judgement').css('display', 'none');
+})
+
+$('#2XJudgement').click(function() {
+    let vid = document.getElementById("Judgement");
+    vid.playbackRate = 2.0;
+})
+
+$('#1.5XJudgement').click(function() {
+    let vid = document.getElementById("Judgement");
+    vid.playbackRate = 1.5;
+})
+
+$('#1XJudgement').click(function() {
+    let vid = document.getElementById("Judgement");
     vid.playbackRate = 1.0;
 })
 
